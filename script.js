632a3a228ad83309b91dea6c6b5d7527cbf91e3e
yuji0507 <yuji.ma57@gmail.com>
2024-05-12 20:12:52 +0800

    perfec332

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const photo = document.getElementById('photo');
 // ボタンがクリックされたときのイベントを設定
 changeButton.addEventListener('click', () => {
     // 新しい画像のURLを設定
-    const newPhotoURL = 'newphoto.jpg';
+    const newPhotoURL = 'https://yuji0507.github.io/.github.io/IMG20240501122057.jpg';
 
     // 画像のsrc属性を新しいURLに変更
-    photo.src = 'https://yuji0507.github.io/.github.io/IMG20240501122057.jpg';
-});
+    photo.src = newPhotoURL;
+    }
+);
